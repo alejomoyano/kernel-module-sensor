1e19a02c1793cb0c86bfce670e6726796f4ff221
Alejo Moyano <alejomoyano16@gmail.com>
2023-06-19 19:37:38 -0300

    codigo para pruebas

--- sensor.c
+++ sensor.c
@@ -1,136 +1,182 @@
 #include <linux/module.h>
-#include <linux/version.h>
-#include <linux/kernel.h>
-#include <linux/types.h>
-#include <linux/kdev_t.h>
+#include <linux/init.h>
 #include <linux/fs.h>
-#include <linux/device.h>
 #include <linux/cdev.h>
-#include <linux/gpio.h> // libreria con la que handleamos los GPIOs
+#include <linux/uaccess.h>
+#include <linux/gpio.h>
 
-MODULE_LICENSE("GPL"); /*  Licencia del modulo */
-MODULE_DESCRIPTION("Sensor");
-MODULE_AUTHOR("Paul Carter's Gang");
+/* Meta Information */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Johannes 4 GNU/Linux");
+MODULE_DESCRIPTION("A simple gpio driver for setting a LED and reading a button");
 
-static dev_t mm_sensor;
-static struct class *sensor_class;
-static struct cdev c_dev;
-struct device *dev_sensor;
+/* Variables for device and device class */
+static dev_t dev_sensor;
+static struct class *class_sensor;
+static struct cdev cdev_sensor;
 
-int selector = 0;
+int signal = 17;
 
-// TODO ->  crear las funciones de cambiar
+#define DRIVER_NAME "gsensors"
+#define DRIVER_CLASS "gsensors_class"
 
-static int sensor_open(struct inode *i, struct file *f)
+/**
+ * @brief Read data out of the buffer
+ */
+static ssize_t sensor_driver_read(struct file *File, char *user_buffer, size_t len, loff_t *offs)
 {
-    printk("Driver Sensores Abierto\n");
+    int to_copy, not_copied, delta;
+    char tmp[3] = " \n";
+
+    /* amount of data to copy */
+    to_copy = min(len, sizeof(tmp));
+
+    /* read value of button */
+    printk("GPIO %d: %d\n", signal, gpio_get_value(signal));
+    tmp[0] = gpio_get_value(signal) + '0';
+
+    /* copy data to user */
+    not_copied = copy_to_user(user_buffer, &tmp, to_copy);
+
+    /* calculate data */
+    delta = to_copy - not_copied;
+
+    return delta;
+}
+
+/**
+ * @brief Write data to buffer
+ */
+static ssize_t sensor_driver_write(struct file *File, const char *user_buffer, size_t len, loff_t *offs)
+{
+    int to_copy, not_copied, delta;
+    char value;
+
+    to_copy = min(len, sizeof(value));
+    not_copied = copy_from_user(&value, user_buffer, to_copy);
+
+    printk("Se escribio en el driver\n");
+
+    switch(user_buffer){
+        case '17':
+            signal = 17;
+            break;
+        case '4':
+            signal = 4;
+            break;
+        default:
+            printk("GPIO no disponible. Ingrese 17 o 4.\n");
+            break;
+    }
+
+    /* calculate data */
+    delta = to_copy - not_copied;
+
+    return delta;
+}
+
+/**
+ * @brief This function is called, when the device file is opened
+ */
+static int sensor_driver_open(struct inode *device_file, struct file *instance)
+{
+    printk("Driver abierto!\n");
     return 0;
 }
-static int sensor_close(struct inode *i, struct file *f)
+
+/**
+ * @brief This function is called, when the device file is opened
+ */
+static int sensor_driver_close(struct inode *device_file, struct file *instance)
 {
-    printk("Driver Sensores Cerrado\n");
+    printk("Driver cerrado!\n");
     return 0;
 }
 
-static ssize_t sensor_read(struct file *f, char __user *buf, size_t len, loff_t *off)
+static struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .open = sensor_driver_open,
+    .release = sensor_driver_close,
+    .read = sensor_driver_read,
+    .write = sensor_driver_write};
+
+/**
+ * @brief This function is called, when the module is loaded into the kernel
+ */
+static int __init ModuleInit(void)
 {
-    int flag = 0;
-    printk("Leyendo el sensor %s\n",selector);
-    // deberia colocar en buf lo que lee del GPIO y len el size en bytes
-    // while(flag){
-        // dormirlo un segundo
-    printk("%i\n",gpio_get_value(selector));
-    // }
-    return len; // ver esto, estaba en 0 antes
-}
-static ssize_t sensor_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
-{
-    // deberia cambiar el sensor, o sea la variable selector.
-    char input;
-    copy_from_user(&input, buf, len)
-        printk("Cambiando a sensor %s\n", input);
+    printk("Montando el driver...\n");
 
-    if (input == '1')
-        selector = 1; // cambiarlo por el numero del GPIO
-    else if (input == '0')
-        selector = 0; // a este tambien
-    else
-        printk("No existe el sensor seleccionado. Ingrese 0 o 1.\n");
-    return len;
-}
-
-static struct file_operations fops =
+    if (alloc_chrdev_region(&dev_sensor, 0, 1, DRIVER_NAME) < 0)
     {
-        .owner = THIS_MODULE,
-        .open = sensor_open,
-        .release = sensor_close,
-        .read = sensor_read,
-        .write = sensor_write};
-
-static int __init sensor_init(void) /* Constructor */
-{
-    int ret;
-
-    // asignacion de un major minor
-    printk(KERN_INFO "Montando modulo de sensado...");
-    ret = alloc_chrdev_region(&mm_sensor, 0, 1, "sensors");
-    if (ret < 0)
-    {
-        return ret;
+        printk("Device Nr. could not be allocated!\n");
+        return -1;
     }
-    printk(KERN_INFO "<Major, Minor>: <%d, %d>\n", MAJOR(mm_sensor), MINOR(mm_sensor));
+    printk("Major: %d y Minor: %d\n", MAJOR(dev_sensor), MINOR(dev_sensor));
 
-    // creamos el archivo en /dev
-    sensor_class = class_create(THIS_MODULE, "chardevsensor");
-    if (IS_ERR(sensor_class))
+    if ((class_sensor = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL)
     {
-        printk(KERN_ALERT "Error al crear la class device\n");
-        return PTR_ERR(sensor_class);
+        printk("Device class can not be created!\n");
+        return -1;
     }
 
-    dev_sensor = device_create(sensor_class, NULL, mm_sensor, NULL, "sensors");
-    if (IS_ERR(dev_sensor))
+    /* create device file */
+    if (device_create(class_sensor, NULL, dev_sensor, NULL, DRIVER_NAME) == NULL)
     {
-        class_destroy(sensor_class);
-        unregister_chrdev_region(mm_sensor, 1);
-        return PTR_ERR(dev_sensor);
+        printk("Can not create device file!\n");
+        return -1;
     }
 
-    cdev_init(&c_dev, &fops);
-    ret = cdev_add(&c_dev, mm_sensor, 1);
-    if (ret < 0)
+    /* dev file */
+    cdev_init(&cdev_sensor, &fops);
+
+    /* Regisering device to kernel */
+    if (cdev_add(&cdev_sensor, dev_sensor, 1) == -1)
     {
-        device_destroy(sensor_class, mm_sensor);
-        class_destroy(sensor_class);
-        unregister_chrdev_region(mm_sensor, 1);
-        return ret;
+        printk("Registering of device to kernel failed!\n");
+        return -1;
     }
 
-    // "pedimos" los pines
+    /* GPIO 4 */
     if (gpio_request(4, "rpi-gpio-4"))
-        printk("Error pidiendo el GPIO\n");
-
-    if (gpio_request(17, "rpi-gpio-17"))
-        printk("Error pidiendo el GPIO\n");
-
-    // seteamos como entrada los pines
+    {
+        printk("Can not allocate GPIO 4\n");
+        return -1;
+    }
     if (gpio_direction_input(4))
-        printk("Error seteando GPIO como entrada\n");
+    {
+        printk("Can not set GPIO 4 to output!\n");
+        return -1;
+    }
 
+    /* GPIO 17 */
+    if (gpio_request(17, "rpi-gpio-17"))
+    {
+        printk("Can not allocate GPIO 17\n");
+        return -1;
+    }
     if (gpio_direction_input(17))
-        printk("Error seteando GPIO como entrada\n");
+    {
+        printk("Can not set GPIO 17 to input!\n");
+        return -1;
+    }
 
     return 0;
 }
 
-static void __exit sensor_exit(void) /* Destructor */
+/**
+ * @brief This function is called, when the module is removed from the kernel
+ */
+static void __exit ModuleExit(void)
 {
-    cdev_del(&c_dev);
-    device_destroy(sensor_class, mm_sensor);
-    class_destroy(sensor_class);
-    unregister_chrdev_region(mm_sensor, 1);
-    printk(KERN_INFO "Desmontando modulo de sensado.");
+    gpio_free(17);
+    gpio_free(4);
+    cdev_del(&cdev_sensor);
+    device_destroy(class_sensor, dev_sensor);
+    class_destroy(class_sensor);
+    unregister_chrdev_region(dev_sensor, 1);
+    printk("Driver desmontado.\n");
 }
 
-module_init(sensor_init);
-module_exit(sensor_exit);
+module_init(ModuleInit);
+module_exit(ModuleExit);
